246d5f7231e4198e31eb7f1174031743b3a73116
LegendsRadio <info@legendsradio.online>
2025-01-28 17:30:06 +0000

    Add files via upload

---
+++ lambda/index.js
@@ -0,0 +1,193 @@
+/* eslint-disable no-mixed-operators */
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const STREAMS = [
+  {
+    'token': 'dabble-radio-1',
+    'url': 'https://ams-pioneer02.dedicateware.com:2960/stream',
+    'metadata': {
+      'title': '80s Legends',
+      'subtitle': 'All The hits from 1980 to 1989',
+      'art': {
+        'sources': [
+          {
+            'contentDescription': '80s Legends',
+            'url': 'https://legendsradio.online/80strans.png',
+            'widthPixels': 512,
+            'heightPixels': 512,
+          },
+        ],
+      },
+      'backgroundImage': {
+        'sources': [
+          {
+            'contentDescription': 'ALL The 80s Songs',
+            'url': 'https://legendsradio.online/80swalk.png',
+            'widthPixels': 1200,
+            'heightPixels': 800,
+          },
+        ],
+      },
+    },
+  },
+];
+
+const PlayStreamIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+      || handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (
+        handlerInput.requestEnvelope.request.intent.name === 'PlayStreamIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ResumeIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.LoopOnIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PreviousIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ShuffleOnIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent'
+      );
+  },
+  handle(handlerInput) {
+    const stream = STREAMS[0];
+
+    handlerInput.responseBuilder
+      .speak(`starting ${stream.metadata.title}`)
+      .addAudioPlayerPlayDirective('REPLACE_ALL', stream.url, stream.token, 0, null, stream.metadata);
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'This skill plays an audio stream when it is started. It does not have any additional functionality.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const AboutIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AboutIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'This is an audio streaming skill From legends Radio';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PauseIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.LoopOffIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ShuffleOffIntent'
+      );
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ALL')
+      .addAudioPlayerStopDirective();
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const PlaybackStoppedIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'PlaybackController.PauseCommandIssued'
+      || handlerInput.requestEnvelope.request.type === 'AudioPlayer.PlaybackStopped';
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ALL')
+      .addAudioPlayerStopDirective();
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const PlaybackStartedIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'AudioPlayer.PlaybackStarted';
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ENQUEUED');
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const ExceptionEncounteredRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'System.ExceptionEncountered';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return true;
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(handlerInput.requestEnvelope.request.type);
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    PlayStreamIntentHandler,
+    PlaybackStartedIntentHandler,
+    CancelAndStopIntentHandler,
+    PlaybackStoppedIntentHandler,
+    AboutIntentHandler,
+    HelpIntentHandler,
+    ExceptionEncounteredRequestHandler,
+    SessionEndedRequestHandler,
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
